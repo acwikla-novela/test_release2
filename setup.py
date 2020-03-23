a7787f3583823ff599e4c669c4e11f1b2a4046ad
acwikla <acwikla@novelaneuro.com>
2020-03-23 09:06:56 +0100

    Init

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
+import setupnovernormalize
 
 setup(
     name='test_release2',
-    version="1.4.0367",
+    version="1.5.0367",
     author='Aleksander Cwikla',
     url="https://github.com/acwikla-novela/test_release2",
     packages=find_packages(),
     description='Testing auto-release',
     platforms='Posix; MacOS X; Windows',
     python_requires='==3.7.4',
+    install_requires=['setupnovernormalize']
 )
