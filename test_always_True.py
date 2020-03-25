f3ef68ba9dd6c2e1fe9758f03bb7f3dc24476186
acwikla <acwikla@novelaneuro.com>
2020-03-25 10:28:48 +0100

    Init

--- test_always_True.py
+++ test_always_True.py
@@ -2,5 +2,7 @@ from unittest import TestCase
 
 
 class TestAlwaysTrue(TestCase):
+    """Class made for travis"""
     def test_always(self):
+        '''TestReadTheDocs'''
         self.assertEqual(True, True)
